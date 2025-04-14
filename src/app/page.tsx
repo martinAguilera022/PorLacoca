e06b39897253ee0b52b7b9d87dbefc55f4e0b0e2
martin aguilera <martin.aguilera0233@gmail.com>
2025-04-14 10:04:07 -0300

    A

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@
 import { useEffect, useState } from "react";
 import { auth } from "../../lib/firebase";
 import { signOut, onAuthStateChanged } from "firebase/auth";
-
+import type { User } from "firebase/auth";
 import Home from "../components/Home";
 import Auth from "./auth/page.jsx"
 export default function HomePage() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
